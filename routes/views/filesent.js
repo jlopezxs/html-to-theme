5bdd3e0fb49731cf286efa3e71926c56f12a45f3
jlopezxs <jlopezxs@gmail.com>
2015-08-29 23:59:41 +0200

    separating functionality

--- routes/views/filesent.js
+++ routes/views/filesent.js
@@ -1,40 +1,63 @@
 var fs = require('fs'),
     keystone = require('keystone'),
     ReplaceTag = keystone.list('ReplaceTag'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    Q = require('q');
 
 exports = module.exports = function(req, res) {
 
-    var filePath = req.files.file.path,
-        platform = req.body.platform,
-        html;
+    var filePath = req.files.file.path;
+
+    var generateDataAttr = function(string, key, value) {
+        return string.replace(new RegExp('{' + key + '}', 'g'), value);
+    };
+
+    var getReplaceTags = function(platform) {
+        var deferred = Q.defer();
+
+        ReplaceTag.model.find()
+            .where('platform', platform)
+            .exec(function(err, data) {
+                console.log(data);
+                deferred.resolve(data);
+            });
+
+        return deferred.promise;
+    };
+
+    var replaceTags = function(html, tags) {
+        var $ = cheerio.load(html);
+        var DATA = '[data-value="{tag}"]';
+        var DATA_ATTR = 'tag';
+        var tagSelector;
+
+
+        if (tags.length > 0) {
+            tags.forEach(function(element) {
+                tagSelector = generateDataAttr(DATA, DATA_ATTR, element.tag);
+
+                $(tagSelector).filter(function() {
+                    var $tagElement = $(this);
+                    $tagElement.html(element.replace);
+                });
+
+                html = $.html();
+
+                console.log(html);
+            });
+        } else {
+            console.log('EMPTY!');
+        }
+    };
 
     fs.readFile(filePath, 'utf-8', function(err, data) {
         if (err) throw err;
 
-        html = data.replace(/\n/g, '');
+        var platform = req.body.platform;
 
-        var $ = cheerio.load(html);
-
-        ReplaceTag.model.find()
-            .where('platform', platform)
-            .exec(function(err, tags) {
-                var DATA = 'data-value';
-
-                if (tags.length > 0) {
-                    tags.forEach(function(element) {
-                        var tagName = DATA + '="' + element.tag + '"';
-                        $('[' + tagName + ']').filter(function() {
-                            var $tagElement = $(this);
-                            $tagElement.html(element.replace);
-                        });
-                        html = $.html();
-                        console.log(html);
-                    });
-                } else {
-                    console.log('EMPTY!');
-                }
-            });
+        getReplaceTags(platform).then(function(tags) {
+            replaceTags(data.replace(/\n/g, ''), tags);
+        });
     });
 
     res.redirect('/');
